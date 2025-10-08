fbbdf48cd46570d9c3267b870ec5a048161336b7
Yuki Matsukura <matsubokkuri@gmail.com>
2025-10-09 02:41:34 +0900

    refactoring fix warning.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: {
+    "postcss-preset-env": {
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
     "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
